0b5a12083371503740007d8076027b2ed7b39e23
weizy0219 <weizy@126.com>
2019-12-14 20:38:49 +0800

    clear to rewrite

--- obsolete/obsolete_test/util_test.js
+++
@@ -1,8 +0,0 @@
-const expect=require('chai').expect;
-
-describe('Project environment test',()=>{
-    it('should pass this canary test',()=>{
-        expect(true).to.eql(true);
-        expect(false).to.eql(false);
-    });
-});
